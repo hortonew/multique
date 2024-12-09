175930e34381acc185b143b00b3e606e0f5ba3c7
Erik Horton <hortonew@gmail.com>
2024-12-09 08:34:06 -0700

    Prepare for linkedin

--- src/posts.rs
+++ src/posts.rs
@@ -3,6 +3,7 @@ pub struct AppState {
     pub twitter_authorized: bool,
     pub mastodon_authorized: bool,
     pub bluesky_authorized: bool,
+    pub linkedin_authorized: bool,
     pub post_text: String,
     pub bluesky_token: Option<String>,
     pub did: Option<String>,
